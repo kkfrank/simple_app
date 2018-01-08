443002bbfa141d75a9ad3a4d254ad8d92d7ac842
frank <360305993@qq.com>
2018-01-08 23:16:18 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+  # debugger
+    @user=User.new
   end
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def create
+    #@user=User.new(params[:user])
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success]="welcome to the Sample APp"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmatoinß)
+    end
 end
